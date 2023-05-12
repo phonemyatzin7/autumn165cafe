4d99291f2846817e17513357b0446d15ea1ca2af
NiCK- <120101803+phonemyatzin7@users.noreply.github.com>
2023-05-12 19:44:02 +0630

    Update app.js
    12.5.23

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
 // jQuery area
-// $(document).ready(function(){
+$(document).ready(function(){
 //     $(".test-popup-link").magnificPopup({
 //         type: 'image'
 //     })
-// });
 
-$(window).scroll(function(){
-    let getscrollpoint = $(this).scrollTop();
-    // console.log(getscrollpoint);
 
-    if(getscrollpoint >= 250){
-        $(".booking").addClass("fromlefts");
-        $(".bookingpic").addClass("fromrights");
-    }else{
-        $(".booking").removeClass("fromlefts");
-        $(".bookingpic").removeClass("fromrights");
-    }
+// start modal-box area
+        $("#bookingbtn").click(function(e){
+            $('#bookingmodal').modal("show");
+
+            e.preventDefault();
+        });
+
+        $("#bookingdate").datepicker({
+            numberofMonths: 1,
+            dateFormat: "dd/mm/yy",
+            minDate: 0,
+            autoHide: true
+        });
+// end modal-box area
+
+
+
+        $(window).scroll(function(){
+            let getscrollpoint = $(this).scrollTop();
+            // console.log(getscrollpoint);
+
+            if(getscrollpoint >= 250){
+            $(".booking").addClass("fromlefts");
+            $(".bookingpic").addClass("fromrights");
+        }else{
+            $(".booking").removeClass("fromlefts");
+            $(".bookingpic").removeClass("fromrights");
+        }
+        });
 });
 
 
@@ -40,7 +58,7 @@ const testimonials = [
 
     {
         content:"Free Wifi",
-        quote:"Best wifi to work to researh to study."
+        quote:"Best wifi to work, to researh, to study."
     },
 
     {
@@ -50,7 +68,7 @@ const testimonials = [
 
     {
         content:"Quiet Place",
-        quote:"Best place to read to work to date."
+        quote:"Best place to read, to work, to date."
     }
 ]
 
@@ -71,3 +89,28 @@ function updatetestimonial(){
 
 setInterval(updatetestimonial,5000);
 // end about section
+
+
+// start contact section
+function tabbox(name,ele){
+    var i,tabcontact;
+    
+    tabcontacts = document.getElementsByClassName('tabcontacts');
+    for(i=0;i<tabcontacts.length;i++){
+        tabcontacts[i].style.display = "none";
+        tabcontacts[i].style.height = "300px";
+    }
+
+    document.getElementById(name).style.display = "block";
+    document.getElementById(name).style.height = "300px";
+}
+
+document.getElementById('autoopen').click();
+// end contact section
+
+
+// start footer
+    let autoyear = document.getElementById('getyear');
+    let getyear = new Date().getUTCFullYear();
+    autoyear.textContent = getyear;
+// end footer
